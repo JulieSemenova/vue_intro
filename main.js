d74784643843e70e1323e8192a4d633c916115ac
Julie Semenova <js@id-media.ru>
2018-10-09 16:55:03 +0300

    ✨ bind image and altText (2nd lesson)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var product = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks'
+    description: 'A pair of warm, fuzzy socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    altText: "awesome socks"
   }
 });
